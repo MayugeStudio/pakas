27d5be2843b3a682dc52854f6aa2796b912a4a03
Mayuge <mayuge.studio.public@gmail.com>
2024-10-06 07:41:03 +0900

    Draw player rectangle

--- main.c
+++ main.c
@@ -12,7 +12,13 @@
 #define TILE_ROWS (DISPLAY_HEIGHT/TILE_SIZE)
 #define TILE_COLS (DISPLAY_WIDTH/TILE_SIZE)
 
+#define LVL_AT(tiles, row, col) (tiles[(row*TILE_COLS)+col])
+
 struct Tile {
+    Rectangle rect;
+};
+
+struct Player {
     Rectangle rect;
 };
 
@@ -20,41 +26,53 @@ int main(void)
 {
     InitWindow(WINDOW_WIDTH, WINDOW_HEIGHT, "PAKAS");
 
-    Image tile_image = LoadImage("assets/01-land.png");
 
-    struct Tile tiles[TILE_ROWS*TILE_COLS] = {0};
+    struct Tile level[TILE_ROWS*TILE_COLS] = {0};
 
     for (int row=0; row<TILE_ROWS; row++) {
         for (int col=0; col<TILE_COLS; col++) {
-            tiles[row*TILE_COLS+col].rect.x = col*TILE_SIZE;
-            tiles[row*TILE_COLS+col].rect.y = row*TILE_SIZE;
-            tiles[row*TILE_COLS+col].rect.width = TILE_SIZE;
-            tiles[row*TILE_COLS+col].rect.height = TILE_SIZE;
-            printf("row: %d, col: %d\n", row, col);
-            printf("x: %0.f, y: %0.f\n", tiles[row*TILE_COLS+col].rect.x, tiles[row*TILE_COLS+col].rect.y);
+            struct Tile *tile = &LVL_AT(level, row, col);
+            tile->rect.x = col*TILE_SIZE;
+            tile->rect.y = row*TILE_SIZE;
+            tile->rect.width = TILE_SIZE;
+            tile->rect.height = TILE_SIZE;
         }
     }
 
-    Image display = GenImageColor(DISPLAY_WIDTH, DISPLAY_HEIGHT, RAYWHITE);
+    Image display_image = GenImageColor(DISPLAY_WIDTH, DISPLAY_HEIGHT, RAYWHITE);
     Rectangle display_rect = { .x=0, .y=0, .width=DISPLAY_WIDTH, .height=DISPLAY_HEIGHT };
-    for (int row=0; row<TILE_ROWS; row++) {
-        for (int col=0; col<TILE_COLS; col++) {
-            struct Tile tile = tiles[row*TILE_COLS+col];
-            ImageDraw(&display, tile_image, display_rect, tile.rect, WHITE);
-        }
-    }
 
-    Texture display_texture = LoadTextureFromImage(display);
+    // Draw background image then unload it.
+    Image bg_image = LoadImage("assets/background.png");
+    ImageDraw(&display_image, bg_image, display_rect, display_rect, WHITE);
+    UnloadImage(bg_image);
+
+    Texture display_texture = LoadTextureFromImage(display_image);
     Vector2 display_position = { .x=0, .y=0 };
 
-    // Unload Images.
-    UnloadImage(display);
-    UnloadImage(tile_image);
+
+    Image tile_image = LoadImage("assets/01-land.png");
 
     SetTargetFPS(60);
 
+    struct Player player = { .rect=(Rectangle){ .x=TILE_SIZE*6+(TILE_SIZE/2), .y=TILE_SIZE*5, .width=TILE_SIZE, .height=TILE_SIZE } };
+
     while(!WindowShouldClose())
     {
+        // Update Display Image
+        {
+            for (int row=6; row<TILE_ROWS; row++) {
+                for (int col=0; col<TILE_COLS; col++) {
+                    ImageDraw(&display_image, tile_image, display_rect, LVL_AT(level, row, col).rect, WHITE);
+                }
+            }
+
+            ImageDrawRectangleRec(&display_image, player.rect, RED);
+        }
+
+        // Update Texture
+        UpdateTexture(display_texture, display_image.data);
+
         BeginDrawing();
         {
             ClearBackground(RAYWHITE);
@@ -62,8 +80,10 @@ int main(void)
         }
         EndDrawing();
     }
-    
-    // Unload Textures.
+
+    // Unload Images and Textures.
+    UnloadImage(tile_image);
+    UnloadImage(display_image);
     UnloadTexture(display_texture);
     CloseWindow();
 }
